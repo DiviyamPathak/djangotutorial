68e68de378386bcbb74d6d61e4641654f819f101
DiviyamPathak <patdiviyam@gmail.com>
2021-12-28 18:56:06 +0530

    2ndcommit

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import tasks
+
+Tasks=["foo", "bar" , "qiz"] 
+
+
 # Create your views here.
-def index(sd):
-    return HttpResponse("hello mkgh")
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks":Tasks
+    })
+
+def add(request):
+    
+    return render(request, "tasks/add.html")
