1e289a1a8fd52d7dfb9fadaaf4aecdd27ac2d3fe
DiviyamPathak <patdiviyam@gmail.com>
2022-01-01 07:44:02 +0530

    newcommits

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,18 @@
+from django import forms
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django import forms
 import tasks
+from django.urls import reverse
+
+
+
 
 Tasks=["foo", "bar" , "qiz"] 
+
+class NewTasksForm(forms.Form):
+    task = forms.CharField(label="Add New Task")
 
 
 # Create your views here.
@@ -14,4 +23,17 @@ def index(request):
 
 def add(request):
     
-    return render(request, "tasks/add.html")
+
+    if request.method == "POST":
+        form = NewTasksForm(request.POST)
+        if form.is_valid:
+            task = form.cleaned_data["task"]
+            Tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request,"tasks:add",{
+            "forms": form
+        })
+    return render(request, "tasks/add.html", {
+        "forms":NewTasksForm()
+    })
